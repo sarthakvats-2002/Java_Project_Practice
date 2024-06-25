221c564f278119813ffea9fd1d837423b902b53e
sarthak.vats <sarthak.vats@fk1089-215020l.fkipl.flipkart.com>
2024-06-25 15:29:49 +0530

    Initialised Java project

---
+++ src/com/flipkart/test/Demo.java
@@ -0,0 +1,9 @@
+package com.flipkart.test;
+
+import java.net.StandardSocketOptions;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
